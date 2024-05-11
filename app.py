d567242f808debdefe43b9bd971258d215e3e3cd
daniel.woodrich@noaa.gov <Daniel.Woodrich@nmfs.local>
2024-05-10 17:32:35 -0700

    keep building. need to find a solution to house parameter key values from dynamic components

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ app.layout = html.Div(id='parent', children=[
         html.Div(
     [
                 html.H2(id='H2_1', children='Select Datasets',
-                    style={'textAlign': 'left', 'marginTop': 20}),
+                    style={'textAlign': 'center', 'marginTop': 20}),
                 html.Hr(style={'marginBottom': 40}),
                 dcc.Checklist(id='dataset-select',
                     options=get_datasets(), # [9:]if f"datasets/" == i[:8]
@@ -81,7 +81,7 @@ app.layout = html.Div(id='parent', children=[
         html.Div(
     [
                 html.H2(id='H2_2', children='Select Mode',
-                    style={'textAlign': 'left','marginTop': 20}),
+                    style={'textAlign': 'center','marginTop': 20}),
                 html.Hr(style={'marginBottom': 40}),
                 html.Div(
                     [
@@ -97,18 +97,79 @@ app.layout = html.Div(id='parent', children=[
         html.Div(
             [
                 html.H2(id='H2_3', children='Select Parameters',
-                    style={'textAlign': 'left','marginTop': 20}),
-                html.Hr(style={'marginBottom': 40})
+                    style={'textAlign': 'center','marginTop': 20}),
+                html.Hr(style={'marginBottom': 40}),
+                html.Div(id = "params-holder"),
             ],style ={"display": "inline-block",'vertical-align': 'top','textAlign': 'right'}),
         ]),
 
     html.Div(
         [
-            html.Hr(style = {'marginTop': 100,"width": "200%"}),
+            html.Hr(style = {'marginTop': 50}),
             html.Button('RUN',id="run-button", style={'textAlign': 'left','vertical-align': 'top','marginLeft': 650}),
             html.Hr()
         ]),
+    html.Div(id = "outputs-holder")
 ])
+
+#refactor to have a single 'parameter holder' as input
+@app.callback(
+Output('outputs-holder', 'children'),
+     Input('run-button', 'n_clicks'),
+     State('toggle-mode', 'value'),
+     State('model-select', 'value'),
+     State('checklist-params', 'value'),
+     State('slider-1-params-name', 'children'),
+     State('slider-1-params', 'value')
+ )
+def run_model(n_clicks,mode,model,checklist_params,slider_1_params_name,slider_1_params):
+
+    params = list(zip([slider_1_params_name],[slider_1_params])) #add to internal lists as use more params inputs
+
+    print(params)
+
+    config_out = "Configuration<br>Run mode:"+ "Inference" if mode else "Training" +"\nRun model: "+model+\
+                                                 "\nChecklist parameters selected: "+"\n\t-".join(checklist_params)+\
+                                                 "\nParameters values selected: "+\
+                                                 "".join(["\n\t"+a+":"+str(b) for (a,b) in params])
+
+    return dcc.Textarea(id='config_report',value = config_out,style={'width': 3000,'height': 3000})
+
+@app.callback(#Output('params-select', 'options'),
+                    #Output('params-select', 'value'),
+                    Output('params-holder', 'children'),
+                    Input('toggle-mode', 'value'),
+                    Input('model-select', 'value')
+)
+def get_parameters(mode,model):
+
+    if model is not None:
+        #inference
+        if mode:
+            #we will ideally want this to read properties of the model object itself to run this.
+            #hopefully will not be particular to a certain model.
+
+            if model == "hello4.hd5":
+                return [dcc.Checklist(id='checklist-params', options=["on_GPU","specific other thing","secret third thing"])]
+            else:
+                return [dcc.Checklist(id='checklist-params', options=["on_GPU"], value=[])]
+        #training
+        else:
+            #this will largely be populated manually
+            if model == "this architecture":
+                #return ["test"],[],dcc.Slider(0, 20, 5,value=10,id='test-conditional-component')
+                return [dcc.Checklist(id='checklist-params', options=["test"], value=[]),
+                        html.Div(id='slider-1-params-name',style={'textAlign': 'left'},children="var1"),
+                        dcc.Slider(0, 20, 5,value=10,id='slider-1-params')]
+            elif model == "that architecture":
+                #return ["on_GPU","other thing"],[],[]
+                return [dcc.Checklist(id='checklist-params', options=["on_GPU","other thing"], value=[])]
+            elif model == "other thing":
+                #return ["on_GPU","other thing","secret third thing"],[],[]
+                return [dcc.Checklist(id='checklist-params', options=["on_GPU","other thing","secret third thing"], value=[])]
+    else:
+        return []
+
 
 @app.callback(
     Output('model-select', 'options'),
