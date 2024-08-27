3400f857e37b09bcaf523fc112e3a2cef175da2c
daniel.woodrich@noaa.gov <Daniel.Woodrich@nmfs.local>
2024-08-26 18:10:14 -0700

    try to get paths to work in test and prod env for static images

--- app/app_constant.py
+++ app/app_constant.py
@@ -19,9 +19,9 @@ app_header = html.Div([
             #and then remove on stop
 
         #html.Div(id="secondline", children = [
-                dcc.Link(html.Img(src="/static/home.png",style={"width":"20px","marginLeft":10,"marginRight":10}),
+                dcc.Link(html.Img(src="../static/home.png",style={"width":"20px","marginLeft":10,"marginRight":10}),
                      href=f"/{os.getenv('APPNAME')}/",style={"display": "inline-block","margin":0}),
-                dcc.Link(html.Img(src="/static/ques7tion-sign.png",style={"width":"20px","marginRight":10}),
+                dcc.Link(html.Img(src="../static/question-sign.png",style={"width":"20px","marginRight":10}),
                         href=f"/{os.getenv('APPNAME')}/help",style={"display": "inline-block"}),
                 html.Div("Number current running jobs:", style={"display": "inline-block", 'textAlign': 'right'}),
                 # "float": "right"
@@ -31,12 +31,12 @@ app_header = html.Div([
                     html.Div(f"Web tool Github release version: {os.getenv('WEBAPP_RELEASE')}",
                              style={"display": "inline-block", 'textAlign': 'right'}),
                     html.A(
-                        html.Img(src="/static/github-sign.png", style={"width": "20px", "marginLeft": 10, "marginRight": 10}),
+                        html.Img(src="../static/github-sign.png", style={"width": "20px", "marginLeft": 10, "marginRight": 10}),
                         href="https://github.com/DanWoodrichNOAA/ftnirs-mlapp/tree/ml-dev", style={"display": "inline-block", "marginRight": 15}, target="_blank"),
                     html.Div(f"ML-codebase Github release version: {os.getenv('MLCODE_RELEASE')}",
                              style={"display": "inline-block", 'textAlign': 'right'}),
                     html.A(
-                        html.Img(src="/static/github-sign.png", style={"width": "20px", "marginLeft": 10, "marginRight": 10}),
+                        html.Img(src="../static/github-sign.png", style={"width": "20px", "marginLeft": 10, "marginRight": 10}),
                         href="https://github.com/michael-zakariaie-noaa/ftnirs-ml-codebase/tree/dan-dev", style={"display": "inline-block", "margin": 0}, target="_blank")
                 ],style={"float": "right"})],style={"height": first_row_height,"display": "inline-block","width":"100%","backgroundColor":"white"})],
             #    ],style={"float":"right"})],
